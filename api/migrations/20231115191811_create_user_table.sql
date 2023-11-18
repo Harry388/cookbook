f14bcd68a2e3a509288e5e9260a187363efaedbd
Harry388 <harrythompson3765@gmail.com>
2023-11-18 15:43:56 +0000

    updated migrations

--- api/migrations/20231115191811_create_user_table.sql
+++ api/migrations/20231115191811_create_user_table.sql
@@ -1,8 +1,8 @@
 create table user (
     id int primary key not null auto_increment,
-    username varchar(255) not null,
+    username varchar(255) not null unique,
     display_name varchar(255) not null,
-    email varchar(255) not null,
+    email varchar(255) not null unique,
     password varchar(255) not null,
     bio varchar(255),
     pfp varchar(255)
--- api/migrations/20231118133049_create_following_table.sql
+++ api/migrations/20231118133049_create_following_table.sql
@@ -2,6 +2,6 @@ create table following (
     user_id int not null,
     following_id int not null,
     primary key (user_id, following_id),
-    foreign key (user_id) references user(id),
-    foreign key (following_id) references user(id)
+    foreign key (user_id) references user(id) on delete cascade,
+    foreign key (following_id) references user(id) on delete cascade
 )
