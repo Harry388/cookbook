a8d4e76e3b0172bf83d023e2eb18aa101baa5740
Harry388 <harrythompson3765@gmail.com>
2023-11-25 15:57:09 +0000

    separated app and login

--- website/src/routes/(app)/+layout.ts
+++ website/src/routes/(app)/+layout.ts
@@ -2,9 +2,6 @@ import { test } from '$lib/auth/auth';
 import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ url }) => {
-    if (url.pathname == '/login') {
-        return;
-    }
     const response = await test();
     if (!response.ok) {
         throw redirect(301, `/login?redirect=${url.pathname}`);
