0890cd709aa822fd5f8a20b76cc93f4585946ad5
Harry388 <harrythompson3765@gmail.com>
2023-11-25 16:49:55 +0000

    navbar and drawer

---
+++ website/src/routes/(app)/+layout.svelte
@@ -0,0 +1,42 @@
+<script>
+
+    import { logout } from '$lib/auth/auth';
+    import { goto } from '$app/navigation';
+
+    async function onLogOut() {
+        const response = await logout();
+        if (response.ok) {
+            goto('/login');
+        }
+    }
+
+</script>
+
+<div class="drawer lg:drawer-open">
+    <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content flex flex-col items-center">
+        <!-- Page content here -->
+        <div class="navbar bg-base-200">
+            <div class="navbar-start">
+                <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+            </div>
+            <div class="navbar-center">
+                <h1 class="font-bold text-3xl">CookBook</h1>
+            </div>
+            <div class="navbar-end">
+
+            </div>
+        </div>
+        <slot />
+    </div> 
+    <div class="drawer-side">
+        <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label> 
+        <ul class="menu flex p-4 w-40 min-h-full bg-base-300 text-base-content">
+            <!-- Sidebar content here -->
+            <li><a href="/">Home</a></li>
+            <li><a href="/user">Profile</a></li>
+            <div class="flex-grow"></div>
+            <li class="place-self-start"><button class="btn btn-ghost" on:click={onLogOut}>Log Out</button></li>
+        </ul>
+    </div>
+</div>
--- website/src/routes/(app)/+page.svelte
+++ website/src/routes/(app)/+page.svelte
@@ -1,23 +1,10 @@
-<script>
 
-    import { logout } from '$lib/auth/auth';
-    import { goto } from '$app/navigation';
 
-    async function onLogOut() {
-        const response = await logout();
-        if (response.ok) {
-            goto('/login');
-        }
-    }
-
-</script>
-
-<div class="hero min-h-screen bg-base-200">
+<div class="hero pt-20 bg-base-100">
     <div class="hero-content flex-col lg:flex-row-reverse">
         <div class="text-center lg:text-left">
             <h1 class="text-5xl font-bold">Welcome to CookBook!</h1>
             <p class="py-6">Chomp chomp</p>
-            <button class="btn btn-primary" on:click={onLogOut}>Log Out</button>
         </div>
     </div>
 </div>
