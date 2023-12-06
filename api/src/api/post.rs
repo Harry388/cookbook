3a36207e8abf12c15d5480fa8306ed07ec15ca01
Harry388 <harrythompson3765@gmail.com>
2023-12-06 13:35:51 +0000

    post payload and empty routes

--- api/src/api/post.rs
+++ api/src/api/post.rs
@@ -1,4 +1,4 @@
-use poem_openapi::{OpenApi, payload::{Json, PlainText}, Object, ApiResponse, param::Path, Tags};
+use poem_openapi::{OpenApi, payload::{Json, PlainText, Attachment}, Object, ApiResponse, param::Path, Tags, Multipart};
 use poem::{web::Data, error::InternalServerError, Result};
 use sqlx::MySqlPool;
 use crate::api::auth::JWTAuthorization;
@@ -11,6 +11,13 @@ enum ApiTags {
 
 // Inputs
 
+#[derive(Debug, Multipart)]
+struct Post {
+    title: String,
+    content: Option<String>,
+    user_id: i64,
+    media: Vec<Upload>
+}
 
 
 // Results
@@ -26,6 +33,30 @@ pub struct PostApi;
 #[OpenApi(prefix_path = "/post", tag = "ApiTags::Post")]
 impl PostApi {
     
-    
+    #[oai(path = "/", method = "post")]
+    async fn create_post(&self, pool: Data<&MySqlPool>, post: Post, auth: JWTAuthorization) -> Result<()> {
+        permission::user::is_user(post.user_id, auth)?;
+        Ok(())
+    }
+
+    #[oai(path = "/:id", method = "get")]
+    async fn get_post(&self) {
+
+    }
+
+    #[oai(path = "/user/:user_id", method = "get")]
+    async fn get_user_posts(&self) {
+
+    }
+
+    #[oai(path = "/:id", method = "put")]
+    async fn update_user(&self) {
+
+    }
+
+    #[oai(path = "/:id", method = "delete")]
+    async fn delete_user(&self) {
+
+    }
 
 }
