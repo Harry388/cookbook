aaa07650dc2855bc60de097343c66337eb6fcc83
Harry388 <harrythompson3765@gmail.com>
2023-12-07 17:04:04 +0000

    moved storage url into env

--- api/src/main.rs
+++ api/src/main.rs
@@ -10,7 +10,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let db_url = env::var("DATABASE_URL")?;
     let pool = MySqlPool::connect(&db_url).await?;
 
-    let storage = DufsStorage::new("http://cookbook-files-1:5000");
+    let storage_url = env::var("STORAGE_URL")?;
+    let storage = DufsStorage::new(&storage_url);
 
     let apis = (
         api::user::UserApi, api::auth::AuthApi, api::post::PostApi
