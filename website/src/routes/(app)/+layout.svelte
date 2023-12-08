10d7f4c036d5684ef5eb49012d8df3d06db875ca
Harry388 <harrythompson3765@gmail.com>
2023-12-08 14:35:40 +0000

    moved title to main layout

--- website/src/routes/(app)/+layout.svelte
+++ website/src/routes/(app)/+layout.svelte
@@ -15,10 +15,6 @@
 
 </script>
 
-<svelte:head>
-    <title>{ $page.data.title ? `${$page.data.title} -` : '' } CookBook</title>
-</svelte:head>
-
 <div class="drawer lg:drawer-open">
     <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
     <div class="drawer-content flex flex-col items-center">
--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
     import "../app.css";
+    import { page } from '$app/stores';
 </script>
+
+<svelte:head>
+    <title>{ $page.data.title ? `${$page.data.title} -` : '' } CookBook</title>
+</svelte:head>
 
 <slot />
