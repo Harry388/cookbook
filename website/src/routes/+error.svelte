175680b2e40a713a85bd3aa54e62cfda461d88ce
Harry388 <harrythompson3765@gmail.com>
2023-12-11 13:41:43 +0000

    created error page

---
+++ website/src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script>
+
+    import { page } from '$app/stores';
+
+</script>
+
+<div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Error: { $page.status }</h1>
+        <p class="py-6">{ $page.error?.message }</p>
+        <a href="/" class="btn btn-primary">Go Home</a>
+      </div>
+    </div>
+</div>
