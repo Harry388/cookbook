dd7cd2eca2c655bb17f5dac3dfb5dc0162fd325d
Harry388 <harrythompson3765@gmail.com>
2023-12-14 16:20:23 +0000

    changed image component

--- website/src/lib/components/util/image.svelte
+++ website/src/lib/components/util/image.svelte
@@ -1,32 +1,40 @@
 <script lang="ts">
 
-    import { get } from '$lib/apiFetch';
-    import { onMount } from 'svelte';
+    // import { get } from '$lib/apiFetch';
+    // import { onMount } from 'svelte';
+
+    // export let height: string = '';
+    // export let src: string;
+    // export let alt: string = '';
+
+    // let url: string | null = null;
+
+    // async function getImage() {
+    //     const response = await get(src).run();
+    //     if (response.ok) {
+    //         const image = await response.blob();
+    //         url = URL.createObjectURL(image);
+    //     }
+    // }
+
+    // onMount(getImage);
+
+    // $: {
+    //     src;
+    //     getImage();
+    // }
+
+    import { PUBLIC_BROWSER_API_URL } from "$env/static/public";
 
     export let height: string = '';
     export let src: string;
     export let alt: string = '';
 
-    let url: string | null = null;
-
-    async function getImage() {
-        const response = await get(src).run();
-        if (response.ok) {
-            const image = await response.blob();
-            url = URL.createObjectURL(image);
-        }
-    }
-
-    onMount(getImage);
-
-    $: {
-        src;
-        getImage();
-    }
+    $: url = `${PUBLIC_BROWSER_API_URL}/${src}`;
 
 </script>
 
-<div style="height: {height}" class="flex justify-center">
+<!-- <div style="height: {height}" class="flex justify-center">
     {#if url}
         <img src={url} {alt} {height}>
     {:else}
@@ -34,4 +42,8 @@
             <span class="loading loading-spinner loading-lg"></span>
         </slot>
     {/if}
+</div> -->
+
+<div style="height: {height}" class="flex justify-center">
+    <img src={url} {alt} {height}>
 </div>
