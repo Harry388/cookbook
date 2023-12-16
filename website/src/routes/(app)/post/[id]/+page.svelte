0393ec6d5f30b5a5a99ea82750464b0bc4c4d07f
Harry388 <harrythompson3765@gmail.com>
2023-12-16 23:27:52 +0000

    delete button in post page

--- website/src/routes/(app)/post/[id]/+page.svelte
+++ website/src/routes/(app)/post/[id]/+page.svelte
@@ -1,12 +1,28 @@
 <script lang="ts">
 
     import PostPreview from '$lib/components/post/postPreview.svelte';
+    import { deletePost } from '$lib/app/post';
+    import { goto } from '$app/navigation';
 
     export let data;
+
+    $: ownsPost = data.post.user_id == data.id;
+
+    async function onDelete() {
+        if (!confirm('Are you sure?')) return;
+        const response = await deletePost(data.post.id);
+        if (response.ok) {
+            goto('/user');
+        }
+    }
 
 </script>
 
 <a class="btn btn-outline" href="/user/{data.post.user_id}">Back</a>
+
+{#if ownsPost}
+    <button class="btn btn-error" on:click={onDelete}>Delete Post</button>
+{/if}
 
 <div class="flex flex-col items-center lg:flex-row  mt-5">
 
