db732f2d51c9377acb4e2bfe064ec780d0c71688
Harry388 <harrythompson3765@gmail.com>
2023-12-16 14:35:45 +0000

    fallback for image component

--- website/src/lib/components/util/image.svelte
+++ website/src/lib/components/util/image.svelte
@@ -1,49 +1,27 @@
 <script lang="ts">
 
-    // import { get } from '$lib/apiFetch';
-    // import { onMount } from 'svelte';
-
-    // export let height: string = '';
-    // export let src: string;
-    // export let alt: string = '';
-
-    // let url: string | null = null;
-
-    // async function getImage() {
-    //     const response = await get(src).run();
-    //     if (response.ok) {
-    //         const image = await response.blob();
-    //         url = URL.createObjectURL(image);
-    //     }
-    // }
-
-    // onMount(getImage);
-
-    // $: {
-    //     src;
-    //     getImage();
-    // }
-
     import { PUBLIC_BROWSER_API_URL } from "$env/static/public";
 
     export let height: string = '';
     export let src: string;
     export let alt: string = '';
 
+    let loaded = false;
+
     $: url = `${PUBLIC_BROWSER_API_URL}/${src}`;
+
+    $: {
+        url;
+        loaded = false;
+    }
 
 </script>
 
-<!-- <div style="height: {height}" class="flex justify-center">
-    {#if url}
-        <img src={url} {alt} {height}>
-    {:else}
+<div style="height: {height}" class="flex justify-center">
+    <img class={!loaded ? 'hidden' : ''} src={url} {alt} {height} on:load={() => loaded = true}>
+    {#if !loaded}
         <slot>
             <span class="loading loading-spinner loading-lg"></span>
         </slot>
     {/if}
-</div> -->
-
-<div style="height: {height}" class="flex justify-center">
-    <img src={url} {alt} {height}>
 </div>
