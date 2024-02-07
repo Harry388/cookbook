16a4c2763e6657c63b74e55a9e1aa10b5667d75c
Harry388 <harrythompson3765@gmail.com>
2024-02-07 14:33:36 +0000

    redirect from community edit if not admin

--- website/src/routes/(app)/community/[id]/edit/+page.ts
+++ website/src/routes/(app)/community/[id]/edit/+page.ts
@@ -1,8 +1,13 @@
 import { getCommunity } from '$lib/app/community';
+import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ params, fetch }) => {
 
     const community = await getCommunity(params.id, fetch);
+
+    if (!community.is_admin) {
+        throw redirect(301, `/community/${community.id}`);
+    }
 
     return {
         community,
