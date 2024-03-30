48cd1cba8a35b679a13b06ad0a1ea7054d67a463
harry388 <harrythompson3765@gmail.com>
2024-03-30 17:42:43 +0000

    confirm util component

---
+++ website/src/lib/components/util/confirm.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    function show() {
+        //@ts-ignore
+        document.getElementById('confirmModal').showModal();
+    }
+
+    function confirm() {
+        dispatch('confirm');
+    }
+
+</script>
+
+<slot {show} />
+<dialog id="confirmModal" class="modal modal-bottom sm:modal-middle">
+    <div class="modal-box">
+        <h3 class="font-bold text-lg">Are you sure?</h3>
+        <div class="modal-action">
+            <form method="dialog">
+                <button class="btn btn-ghost mr-5">Cancel</button>
+                <button class="btn btn-error" on:click={confirm}>Delete</button>
+            </form>
+        </div>
+    </div>
+    <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+    </form>
+</dialog>
