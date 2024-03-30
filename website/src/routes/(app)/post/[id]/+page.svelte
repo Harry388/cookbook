a26c3bd9ebc47ebd16158f8009b55addc249e7e4
harry388 <harrythompson3765@gmail.com>
2024-03-30 12:07:28 +0000

    headers on post and recipe pages

--- website/src/routes/(app)/post/[id]/+page.svelte
+++ website/src/routes/(app)/post/[id]/+page.svelte
@@ -11,14 +11,17 @@
 <div class="flex flex-col items-center lg:items-start lg:flex-row  mt-5 lg:justify-start justify-center gap-4">
 
     <div class="basis-1/3">
+        <h3 class="font-bold text-lg pb-5">Post</h3>
         <Post post={data.post} />
     </div>
 
     <div class="basis-1/3">
+        <h3 class="font-bold text-lg pb-5">Recipes</h3>
         <Recipes recipes={data.recipes} />
     </div>
 
     <div class="basis-1/3">
+        <h3 class="font-bold text-lg pb-5">Comments</h3>
         <CommentBlock comments={data.comments} type="POST" id={data.post.id} />
     </div>
 
--- website/src/routes/(app)/recipe/[id]/+page.svelte
+++ website/src/routes/(app)/recipe/[id]/+page.svelte
@@ -11,14 +11,17 @@
 <div class="flex flex-col items-center lg:items-start lg:flex-row  mt-5 justify-center gap-4">
 
     <div class="basis-1/3">
+        <h3 class="font-bold text-lg pb-5">Recipe</h3>
         <Recipe recipe={data.recipe} />
     </div>
 
     <div class="basis-1/3">
+        <h3 class="font-bold text-lg pb-5">Posts</h3>
         <Posts posts={data.posts} />
     </div>
 
     <div class="basis-1/3">
+        <h3 class="font-bold text-lg pb-5">Comments</h3>
         <CommentBlock comments={data.comments} type="RECIPE" id={data.recipe.id} />
     </div>
 
