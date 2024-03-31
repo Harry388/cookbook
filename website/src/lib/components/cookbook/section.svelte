e41da4ee3f0d127e206caefe6f7cb014bf8a59e9
harry388 <harrythompson3765@gmail.com>
2024-03-31 18:38:12 +0100

    section padding

--- website/src/lib/components/cookbook/section.svelte
+++ website/src/lib/components/cookbook/section.svelte
@@ -6,7 +6,7 @@
 
 </script>
 
-<div class="hero pt-80 bg-base-200">
+<div class="hero lg:pt-80 print:pt-80 bg-base-200">
     <div class="hero-content text-center">
         <div class="max-w-md">
             <h1 class="text-5xl font-bold">{ section.position + 1}. { section.title }</h1>
