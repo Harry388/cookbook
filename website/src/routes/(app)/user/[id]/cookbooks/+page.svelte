9aa510f97f018326fcbb1455dd9fe2716d3fa602
harry388 <harrythompson3765@gmail.com>
2024-04-06 19:19:10 +0100

    formatting

--- website/src/routes/(app)/user/[id]/cookbooks/+page.svelte
+++ website/src/routes/(app)/user/[id]/cookbooks/+page.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div class="lg:w-1/3 lg:m-auto flex flex-col">
-    {#if data.id == data.user.id}
+    {#if data.self}
         <a href="/cookbook/create" class="btn btn-outline"><i class="fa-solid fa-plus"></i>Create Cookbook</a>
     {/if}
     <Cookbooks cookbooks={data.cookbooks} />
