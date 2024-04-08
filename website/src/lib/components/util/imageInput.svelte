e291b2e6651139f77036661b989d89c61521a3e4
harry388 <harrythompson3765@gmail.com>
2024-04-08 19:21:05 +0100

    videos on media input

--- website/src/lib/components/util/imageInput.svelte
+++ website/src/lib/components/util/imageInput.svelte
@@ -10,20 +10,25 @@
 
     let input: HTMLInputElement;
 
-    $: images = files.map(file => URL.createObjectURL(file));
+    $: urls = files.map(file => URL.createObjectURL(file));
 
     function onDelete(index: number) {
-        URL.revokeObjectURL(images[index]);
+        URL.revokeObjectURL(urls[index]);
         files = files.filter((_, i) => index != i);
+    }
+
+    function fileType(file: File) {
+        return file.type.split('/')[0];
     }
 
     function onChange() {
         if (input.files) {
+            const newFiles = [...input.files].filter(f => ['image', 'video'].includes(fileType(f)));
             if (multiple) {
-                files = [...files, ...input.files];
+                files = [...files, ...newFiles];
             }
             else {
-                files = [...input.files];
+                files = [...newFiles];
             }
             input.files = null;
         }
@@ -31,21 +36,29 @@
     }
 
     onDestroy(() => {
-        for (const image of images) {
-            URL.revokeObjectURL(image);
+        for (const url of urls) {
+            URL.revokeObjectURL(url);
         }
     })
 
 </script>
 
 <div class="flex flex-wrap gap-5 mb-5">
-    {#each images as image, i}
+    {#each files as file, i}
+        {@const url = urls[i]}
         <div class="self-start indicator w-fit lg:w-1/4">
             <button on:click={() => onDelete(i)} class="indicator-item badge badge-error text-base py-3"><i class="fa-solid fa-xmark"></i></button> 
-            <img src={image} alt="Post" class="rounded-lg shadow-lg">
+            {#if fileType(file) == 'image'}
+                <img src={url} alt="Post" class="rounded-lg shadow-lg">
+            {:else if fileType(file) == 'video'}
+                <video src={url} class="rounded-lg shadow-lg" controls autoplay loop>
+                    <track kind="captions">
+                    <source src={url}>
+                </video>
+            {/if}
         </div>
     {/each}
 </div>
 
 <label for="files" class="btn btn-outline">{ multiple ? 'Add Images' : 'Select Image' }</label>
-<input bind:this={input} on:change={onChange} {multiple} id="files" type="file" accept="image/*" class="hidden">
+<input bind:this={input} on:change={onChange} {multiple} id="files" type="file" accept="image/*,video/*" class="hidden">
