9758d548e40241c93e24ba52e871f9f016a9828b
harry388 <harrythompson3765@gmail.com>
2024-08-13 20:26:33 +0100

    url util formats

--- templates/util/util.go
+++ templates/util/util.go
@@ -2,12 +2,13 @@ package util
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/a-h/templ"
 )
 
-func Url(path string) string {
-    return string(templ.URL(path))
+func Url(format string, a ...any) string {
+    return string(templ.URL(fmt.Sprintf(format, a...)))
 }
 
 func IsLoggedIn(ctx context.Context) bool {
